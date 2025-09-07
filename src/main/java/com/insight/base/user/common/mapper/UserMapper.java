aad80bb9499a81a41aa7565f3cec698e818b805a
Brian <xan.brian@gmail.com>
2025-09-08 01:32:02 +0800

    fix(user): 修复用户列表查询时的无效字段问题
    - 在 UserMapper 中更新了查询语句，使用 ifnull 函数处理 a.invalid 和 u.invalid
    - 这个修改确保了在查询用户列表时，即使 a 表中的无效字段为空，也能正确返回 u表中的无效字段值

--- src/main/java/com/insight/base/user/common/mapper/UserMapper.java
+++ src/main/java/com/insight/base/user/common/mapper/UserMapper.java
@@ -26,7 +26,7 @@ public interface UserMapper {
     @Select("""
             <script>
             select u.id, group_concat(t.name) as tenant, u.type, u.name, u.account, u.mobile, u.email, r.role_name, r.role_id,
-                   u.invalid, u.creator, u.created_time
+                   ifnull(a.invalid, u.invalid) as invalid, u.creator, u.created_time
             from ibu_user u
               <if test = 'longSet != null and longSet.size() > 0'>
               join ibo_organize_member m on m.user_id = u.id and m.post_id in
